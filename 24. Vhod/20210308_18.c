944b4b867be584f72436ac5b072366f2c3c0b9af
Aleksandar Tishev <atishevlol@gmail.com>
2021-03-10 10:10:01 +0200

    upload

--- 24. Vhod/20210308_18.c
+++ 24. Vhod/20210308_18.c
@@ -12,16 +12,19 @@ typedef struct{
 }t_person;
 #include <stdio.h>
 #include <stdlib.h>
+static const char *FORMAT_PERSON_IN = "<person>\n<name>%[^<]</name>\n<age>%d</age>\n<gender>%c</gender>\n</person>\n";
+static const char *FORMAT_PERSON_OUT = "<person>\n<name>%s</name>\n<age>%d</age>\n<gender>%c</gender>\n</person>\n";
+
 int main(void){
     FILE *fp = NULL;
-    fp = fopen("person.dat", "w+");
+    fp = fopen("person.xml", "w+");
     if(NULL == fp) perror("Cant open!");
     t_person Pesho = {"Pesho", 23, 'M'};
     t_person copy;
-    fprintf(fp, "%s, %d, %c", Pesho.name, Pesho.age, Pesho.gender);
+    fprintf(fp, FORMAT_PERSON_OUT, Pesho.name, Pesho.age, Pesho.gender);
     fseek(fp, 0 , SEEK_SET);
-    fscanf(fp, "%[^,], %d, %c", copy.name, &copy.age, &copy.gender);
-    printf("(%s %d %c)\n", copy.name, copy.age, copy.gender);
+    fscanf(fp, FORMAT_PERSON_IN, copy.name, &copy.age, &copy.gender);
+    printf("%s %d %c\n", copy.name, copy.age, copy.gender);
     fclose(fp);
     return 0;
 
